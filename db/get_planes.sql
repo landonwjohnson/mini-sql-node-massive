1394cc0ea0848de0ed8226f0b827f5746a03207e
landonwjohnson <landonwjohnson@gmail.com>
2017-10-05 19:35:14 -0600

    finished project

--- db/get_planes.sql
+++ db/get_planes.sql
@@ -1,1 +1,3 @@
-SELECT * FROM airplanes;
+SELECT * FROM airplanes WHERE PassengerCount > $1;
+
+--Dollar Sign means Index, Index starts at 1, not zero
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config();
+const controller = require('./controller.js');
 
 const app = express();
+massive( process.env.CONNECTION_STRING ).then( dbInstance =>
+    { app.set('db', dbInstance) 
+
+
+
+    })
+
+app.get('/api/planes', controller.getPlanes);
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
+
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
-
